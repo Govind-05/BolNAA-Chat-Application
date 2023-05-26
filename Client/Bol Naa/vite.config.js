92a78f5eec6f1c1b14a9a45d895e4a5ac46606e3
Govind-05 <govindbhardwaj501@gmail.com>
2023-05-26 19:40:08 +0530

    change in vite config file

--- Client/Bol Naa/vite.config.js
+++ Client/Bol Naa/vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as dotenv from 'dotenv';
+dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server:{
     proxy: {
       '/post': {
-           target: 'http://localhost:3000',
+           target: process.env.REACT_APP_PROXY_DOMAIN,
            changeOrigin: true,
            secure: false,      
            ws: true,
